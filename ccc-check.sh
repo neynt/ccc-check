6f524d1c608cb86a4e3b1031eaf6e4906a705427
Jim Zhang <hyriodula@gmail.com>
2014-10-08 10:56:19 -0400

    add colors to output

--- ccc-check.sh
+++ ccc-check.sh
@@ -3,10 +3,12 @@
 # Usage: ccc-check [command] [problem]
 # e.g. ccc-check "python 2012-s2.py" 2012/s2
 
-curdir=$(dirname $0)
-curdir='/vault/code/ccc-check'
+curdir="$(dirname "$(test -L "$0" && readlink "$0" || echo "$0")")"
 num_cases=0
 correct_cases=0
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+CLEAR='\e[0m'
 for infile in $(find $curdir/$2.*.in); do
     let num_cases+=1
     outfile=${infile%%.in}.out
@@ -19,11 +21,11 @@ for infile in $(find $curdir/$2.*.in); do
     ret=$?
 
     if [ $ret -eq 124 ]; then
-        echo "timed out."
+        echo -e "${RED}timed out.${CLEAR}"
         echo "test case copied to $(basename $infile)"
         cp $infile ./$(basename $infile)
     elif [ $ret -ne 0 ]; then
-        echo "failed."
+        echo -e "${RED}failed.${CLEAR}"
         echo "test case copied to $(basename $infile)"
         cp $infile ./$(basename $infile)
         #cat $infile
@@ -32,7 +34,7 @@ for infile in $(find $curdir/$2.*.in); do
         echo "actual output:"
         cat /tmp/ccc-check
     else
-        echo "correct!"
+        echo -e "${GREEN}correct!${CLEAR}"
         let correct_cases+=1
     fi
 done
